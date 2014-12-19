f46269d98ea3dfe5b9be7031e1a4fcc2aaa8556e
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-12-19 13:24:30 -0800

    evalAsync - Deferred Execution

--- src/scope.js
+++ src/scope.js
@@ -4,6 +4,7 @@
 function Scope() {
 	this.$$watchers = [];
 	this.$$lastDirtyWatch = null; 
+	this.$$asyncQueue = [];
 }
 
 function initWatchVal() { }
@@ -32,6 +33,10 @@ Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
 
 Scope.prototype.$eval = function(expr, locals) {
 	return expr(this, locals);
+};
+
+Scope.prototype.$evalAsync = function(expr) {
+	this.$$asyncQueue.push({scope: this, expression: expr}); 
 };
 
 Scope.prototype.$apply = function(expr) {
@@ -68,6 +73,10 @@ Scope.prototype.$digest = function() {
 	var dirty;
 	this.$$lastDirtyWatch = null; 
 	do {
+		while (this.$$asyncQueue.length) {
+			var asyncTask = this.$$asyncQueue.shift();
+			asyncTask.scope.$eval(asyncTask.expression); 
+		}
 		dirty = this.$$digestOnce();
 		if ( dirty && !(ttl--) ) {
 			throw "10 digest iterations reached";
