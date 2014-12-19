1003951aac15f1f1a6bf1992675f968f4494d13b
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-12-19 12:51:10 -0800

    Value-Based Dirty Checking

--- src/scope.js
+++ src/scope.js
@@ -8,15 +8,24 @@ function Scope() {
 
 function initWatchVal() { }
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
 
 	var watcher = {
 		watchFn: watchFn, 
 		listenerFn: listenerFn || function() { }, 
+		valueEq: !!valueEq,
 		last: initWatchVal
 	};
 	this.$$watchers.push(watcher); 
 	this.$$lastDirtyWatch = null;
+};
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+	if (valueEq) {
+		return _.isEqual(newValue, oldValue); 
+	} else {
+		return newValue === oldValue;
+	}
 };
 
 Scope.prototype.$$digestOnce = function() {
@@ -26,9 +35,9 @@ Scope.prototype.$$digestOnce = function() {
 	_.forEach(this.$$watchers, function(watcher) {
 		newValue = watcher.watchFn(self); 
 		oldValue = watcher.last;
-		if ( newValue !== oldValue ) {
+		if ( !self.$$areEqual(newValue, oldValue, watcher.valueEq) ) {
 			self.$$lastDirtyWatch = watcher; 
-			watcher.last = newValue; 
+			watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue); 
 			watcher.listenerFn(newValue, 
 				(oldValue === initWatchVal ? newValue : oldValue),
 				self); 
