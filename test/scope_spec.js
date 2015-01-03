55324fd74e76ad0593c992326a668021428ebc8e
Carl Goldberg <carlgoldberg1236@gmail.com>
2015-01-03 14:40:27 -0800

    Attribute shadowing

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -705,5 +705,27 @@ describe('Scope', function () {
 			expect(aaa.anotherValue).toBeUndefined(); 
 
 		});
+
+		it("shadows a parent's property with the same name", function() {
+			var parent = new Scope();
+			var child = parent.$new();
+
+			parent.name = 'Joe';
+			child.name = 'Jill';
+
+			expect(child.name).toBe('Jill'); 
+			expect(parent.name).toBe('Joe'); 
+		});
+
+		it("does not shadow members of parent scope's attributes", function() {
+			var parent = new Scope();
+			var child = parent.$new();
+
+			parent.user = {name: 'Joe'};
+			child.user.name = 'Jill';
+
+			expect(child.user.name).toBe('Jill'); 
+			expect(parent.user.name).toBe('Jill'); 
+		});
 	});
 });
