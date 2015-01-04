99a956f96c97a40a3024b9726fd02b403c553b3a
Carl Goldberg <carlgoldberg1236@gmail.com>
2015-01-04 13:57:44 -0800

    Separate watches

--- src/scope.js
+++ src/scope.js
@@ -15,6 +15,7 @@ Scope.prototype.$new = function() {
 	var ChildScope = function() { };
 	ChildScope.prototype = this;
 	var child = new ChildScope(); 
+	child.$$watchers = []; 
 	return child;
 };
 
