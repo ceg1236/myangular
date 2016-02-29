c4c03711b70e8e9df1ff66be101982b570fafbd6
Carl Goldberg <cgoldberg@zendesk.com>
2016-02-29 15:41:17 -0800

    Making literals and constants

--- src/parse.js
+++ src/parse.js
@@ -47,7 +47,8 @@ Lexer.prototype.readNumber = function() {
 	number = 1 * number;
 	this.tokens.push({
 		text: number,
-		fn: _.constant(number)
+		fn: _.constant(number),
+		json: true
 	});
 };
 
@@ -57,6 +58,16 @@ function Parser(lexer) {
 
 Parser.prototype.parse = function(text) {
 	this.tokens = this.lexer.lex(text);
-	return _.first(this.tokens).fn;
+	return this.primary();
+};
+
+Parser.prototype.primary = function() {
+	var token = this.tokens[0];
+	var primary = token.fn;
+	if (token.json) {
+		primary.constant = true;
+		primary.literal = true;
+	}
+	return primary;
 };
 
