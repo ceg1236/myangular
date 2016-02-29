4046c75ff7808af917a3fbeba74b1516a98df222
Carl Goldberg <cgoldberg@zendesk.com>
2016-02-29 15:34:03 -0800

    Parsing Integers

--- src/parse.js
+++ src/parse.js
@@ -3,7 +3,7 @@
 
 function parse(expr) {
 	var lexer = new Lexer();
-	var parser = new Parser();
+	var parser = new Parser(lexer);
 	return parser.parse(expr);
 }
 
@@ -12,14 +12,51 @@ function Lexer() {
 }
 
 Lexer.prototype.lex = function(text) {
+	this.text = text;
+	this.index = 0;
+	this.ch = undefined;
+	this.tokens = [];
+	while (this.index < this.text.length) {
+		this.ch = this.text.charAt(this.index);
+		if (this.isNumber(this.ch)) {
+			this.readNumber();
+		} else {
+			throw 'Unexpected next character: ' + this.ch;
+		}
+		this.tokens.push(this.text[this.index]);
+	}
 
+	return this.tokens;
 };
 
-function Parser() {
+Lexer.prototype.isNumber = function(ch) {
+	return '0' <= ch && ch <= '9';
+};
 
+Lexer.prototype.readNumber = function() {
+	var number = '';
+	while (this.index < this.text.length) {
+		var ch = this.text[this.index];
+		if ( this.isNumber(ch) ) {
+			number += ch;
+		} else {
+			break;
+		}
+		this.index++;
+	}
+	number = 1 * number;
+	this.tokens.push({
+		text: number,
+		fn: _.constant(number)
+	});
+};
+
+function Parser(lexer) {
+	this.lexer = lexer;
 }
 
 Parser.prototype.parse = function(text) {
 	this.tokens = this.lexer.lex(text);
+	return _.first(this.tokens).fn;
 };
 
---
+++ test/parse_spec.js
@@ -0,0 +1,12 @@
+/* jshint globalstrict: true */
+/* global parse: false */
+'use strict';
+
+describe("parse", function() {
+	it("can parse an integer", function() {
+		var fn = parse('42');
+		expect(fn).toBeDefined();
+		expect(fn()).toBe(42);
+	});
+
+});
